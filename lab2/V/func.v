4da2cfc6898774735d77a9e4d122c866b83f506b
abc1929 <abc1929@gmail.com>
2015-09-21 18:01:36 -0400

    Update func.v

--- lab2/V/func.v
+++ lab2/V/func.v
@@ -5,16 +5,16 @@ module func(clk, k, g, m, out);
 
 	input clk;
 	input [7:0] k, g, m;
-	output reg [11:0] out;
+	output reg [7:0] out
 	
-	reg [15:0] r = 0;
+	reg [7:0] r = 0;
 	
 	always @(posedge clk)
 		begin
-			if (g > m) r = (k / 'b11111111) * (g - m);
-			else r = (k / 'b11111111) * (m - g);
+			if (g > m) r = (k * (g - m))/('b11111111);
+			else r = (k * (m - g))/('b11111111);
 			if (g == 0) r = 0;
-			out = r[15:4];
+			out = r;
 		end
 
 endmodule
