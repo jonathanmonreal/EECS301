53b6a46cbb9eeddb4a240a7122f5426a718e80a5
abc1929 <abc1929@gmail.com>
2015-09-21 18:02:15 -0400

    Update pwm.v

--- lab2/V/pwm.v
+++ lab2/V/pwm.v
@@ -1,19 +1,22 @@
 module pwm (
     input clk,
     input rst,
-    input [11:0] compare,
+    input [7:0] compare,
     output m
   );
    
   reg pwm_d, pwm_q;
-  reg [11:0] ctr_d, ctr_q;
+  reg [7:0] ctr_d;
+  reg [7:0] ctr_q ;
+  reg [7:0] process;
    
   assign m = pwm_q;
-   
+  assign process[6:0]  = compare[6:0];
+  
   always @(*) begin
-    ctr_d = ctr_q + 1'b1;
-     
-    if (compare > ctr_q)
+    ctr_d = ctr_q + 1'b1; 
+	 process = process + 'b10000000;
+    if (process > ctr_q)
       pwm_d = 1'b1;
     else
       pwm_d = 1'b0;
@@ -30,3 +33,4 @@ module pwm (
   end
    
 endmodule
+
